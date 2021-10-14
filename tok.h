45acc95e59941ef3a4eaade3ea788c33745e26cc
JaviBT <javibarrancotena@gmail.com>
2021-10-14 15:05:52 +0200

    archivos base - practica 2

---
+++ tok.h
@@ -0,0 +1,20 @@
+/******************************************************
+Nombre: tok.h
+Descripción: Definición de tokens - ejemplo
+******************************************************/
+#ifndef _TOK_H
+#define _TOK_H
+
+
+/* Palabras reservadas */ 
+#define TOK_INICIO                1000
+#define TOK_FIN                   1001
+#define TOK_ENTERO                1002
+#define TOK_VECTOR                1003
+
+#define TOK_ID                   300
+
+/* Errores */
+#define TOK_ERROR                -1
+
+#endif
